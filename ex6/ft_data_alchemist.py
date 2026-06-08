176792b83adf62fef2ba331e232f6e7e637928dc
bgungor44 <bilalyasirg@gmail.com>
2026-06-08 23:19:48 +0000

    module completed

--- ex6/ft_data_alchemist.py
+++ ex6/ft_data_alchemist.py
@@ -1,7 +1,6 @@
 import random
 
 
-
 def main() -> None:
     print("=== Game Data Alchemist ===")
 
@@ -17,17 +16,14 @@ def main() -> None:
         "Liam",
     ]
 
-
     capitalized_players: list[str] = [name.capitalize() for name in players]
     already_capitalized: list[str] = [
         name for name in players if name[0].isupper()
     ]
 
-
     print(f"Initial list of players: {players}")
     print(f"New list with all names capitalized: {capitalized_players}")
     print(f"New list of capitalized names only: {already_capitalized}")
-
 
     scores: dict[str, int] = {
         name: random.randint(0, 1000) for name in capitalized_players
@@ -37,13 +33,15 @@ def main() -> None:
         sum([scores[name] for name in scores]) / len(scores),
         2
     )
+
     high_scores: dict[str, int] = {
-        name: score for name, score in scores.items() if score > average
+        name: scores[name] for name in scores if scores[name] > average
     }
 
     print(f"Score dict: {scores}")
     print(f"Score average is {average}")
     print(f"High scores: {high_scores}")
+
 
 if __name__ == "__main__":
     main()
